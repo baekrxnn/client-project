a0673f6210b07ebca5479c42a514abcf2d1cee70
Karen He <hainechoko@gmail.com>
2019-03-22 01:28:14 +0000

    started making date thing

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // JavaScript File
+/*global $*/
+
+let today = new Date();
+let currentYear=(today.prototype.getFullYear()).toString();
+let currentMonth=(today.prototype.getMonth()+1).toString();
+let currentDate=(today.prototype.getDate()).toString();
+
+let date = currentYear + "." + currentMonth + "." +currentDate;
+console.log(date);
+
+let day=(Date()).toString;
+console.log(day);
+
+$("#date").text(day);
