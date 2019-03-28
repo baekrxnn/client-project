92d8f23d0319d20cf3ae4fb4039af2b6704a4416
Karen He <hainechoko@gmail.com>
2019-03-28 00:08:23 +0000

    current time will now be appended to html

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 /*global $*/
 
 let today = new Date();
+// get current date
 let currentYear=(today.getFullYear()).toString();
 let currentMonth=(today.getMonth()+1).toString();
 let currentDate=(today.getDate()).toString();
@@ -10,3 +11,18 @@ let date = currentYear + "." + currentMonth + "." +currentDate;
 console.log(date);
 
 $("#date").text(date);
+
+// get current time
+let currentHour=(today.getHours()).toString();
+let currentMin=(today.getMinutes()).toString();
+if (currentMin <10) {
+    currentMin="0"+currentMin;
+}
+//console.log(currentMin);
+
+let time = currentHour + ":" + currentMin;
+console.log(time);
+
+$("#time").text(time);
+
+//console.log(new Date().toLocaleTimeString()); 
